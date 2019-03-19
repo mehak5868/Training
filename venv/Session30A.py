808304be8d31fdb7233515618ebb7828d7dd90e8
Mehak <mehakapoor6090@gmail.com>
2019-03-19 20:45:03 +0530

    initial commit

--- venv/Session30A.py
+++ venv/Session30A.py
@@ -1,2 +1,69 @@
 from sklearn import tree
+import pandas as pd
+#X=[1,2,3,4,5]
+#Y=[2,4,5,4,5]
+data = pd.read_csv("advertising.csv")
+X = data["TV"].values
+Y = data["Sales"].values
+x1=0
+y1=0
+#mean of X
+for x in X :
+    x1=x1+x
+MX=x1//len(X)
+print("mean of X:",MX)
+
+#mean of Y
+for y in Y :
+    y1=y1+y
+MY=y1//len(Y)
+print("mean of Y:",MY)
+
+#X-MX
+S =[]
+for x in X :
+    x1 = x - MX
+    S.append(x1)
+print("X-MX :",S)
+
+#Y-MY
+S1 =[]
+for y in Y :
+    y1 = y -MY
+    S1.append(y1)
+print("Y-MY :",S1)
+
+#square of (X-MX)
+S2 =[]
+for mx in S :
+    x1 = mx **2
+    S2.append(x1)
+print("Sq(X-MX):",S2)
+
+#(X-MX)(Y-MY)
+S3 =[]
+for x in range (0,len(S)):
+    S3.append(S[x]*S1[x])
+print("(X-MX)(Y-MY):",S3)
+
+#Sum of square of (X-MX)
+w=0
+for s in S2:
+    w= w + s
+print("Sum of square of (X-MX):",w)
+
+#Sum of (X-MX)(Y-MY)
+w1=0
+for s in S3 :
+    w1 = w1 + s
+print("Sum of (X-MX)(Y-MY):",w1)
+
+b1 = w1/w
+print("b1:",b1)
+
+b0 = MY - (b1*MX)
+print("b0:",b0)
+
+
+
 
--- venv/Session31.py
+++ venv/Session31.py
@@ -32,4 +32,29 @@ for x in X:
     Y1.append(y)
 print(Y1)
 
-plt.xlabel(x)
+
+#plt.xlabel("X")
+#plt.ylabel("Y")
+#plt.grid(True)
+#plt.plot(X,Y,"o",X,Y1)
+#plt.show()
+X=X.reshape(len(X),1)
+Y=X.reshape(len(Y),1)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+regression = LinearRegression()
+regression.fit(X,Y)
+y2= regression.predict(X)
+b0=regression.intercept_
+b1=regression.coef_
+
+r2err=r2_score(Y,y2)
+print("Squared error :",r2err)
+plt.scatter(X,Y,color="black")
+plt.scatter(X,y2,color="blue")
+plt.show()
+
+
+
+
