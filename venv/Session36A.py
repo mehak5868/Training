b7a7c44ab9afd45ce0a44c11e69e4a04cf791db0
Mehak <mehakapoor6090@gmail.com>
2019-03-29 00:14:31 +0530

    project

--- venv/Session36A.py
+++ venv/Session36A.py
@@ -16,4 +16,20 @@ class Dbhelper:
         cursor = con.cursor()
         cursor.execute(sql)
         con.commit()
-##
+
+class prime:
+    def __init__(self,standard,english,maths,hindi,punjabi,evs):
+        self.standard=standard
+        self.english=english
+        self.maths=maths
+        self.hindi=hindi
+        self.punjabi=punjabi
+        self.evs=evs
+
+class Dbhelper1():
+    def saveMarksPrimary(self,sref):
+        sql = "insert into primacademics  values ({},{},{},{},{},{})".format(sref.standard, sref.english, sref.maths,sref.hindi, sref.punjabi, sref.evs)
+        con = mysql.connector.connect(user="root", password="", host="127.0.0.1", database="student")
+        cursor = con.cursor()
+        cursor.execute(sql)
+        con.commit()
