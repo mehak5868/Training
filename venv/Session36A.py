cd3d071f603f42526f0fca23e239a171b902b77e
Mehak <mehakapoor6090@gmail.com>
2019-03-28 22:44:19 +0530

    project

--- venv/Session36A.py
+++ venv/Session36A.py
@@ -1,6 +1,6 @@
 import mysql.connector
 #from Session36 import *
-class Student:
+class Students:
     def __init__(self,standard,english,maths,hindi,punjabi,sst,sci):
         self.standard=standard
         self.english=english
@@ -11,7 +11,7 @@ class Student:
         self.sci=sci
 class Dbhelper:
     def saveMarks(self,sref):
-        sql="insert into Academics values (null,{},{},{},{},{},{})".format(sref.standard,sref.english,sref.maths,sref.hindi,sref.punjabi,sref.sst,sref.sci)
+        sql="insert into Academics values ({},{},{},{},{},{},{})".format(sref.standard,sref.english,sref.maths,sref.hindi,sref.punjabi,sref.sst,sref.sci)
         con = mysql.connector.connect(user="root", password="", host="127.0.0.1", database="student")
         cursor = con.cursor()
         cursor.execute(sql)
